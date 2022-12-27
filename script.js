b98b7e08fc9093cc7c34fec5f0d26617163cd5b8
hirusenevirathne <107979195+hirusenevirathne@users.noreply.github.com>
2022-12-27 14:08:08 +0530

    add comments

--- script.js
+++ script.js
@@ -8,20 +8,23 @@
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highScore = 0;
 
-//Button click event
+//Check Button click event
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(typeof guess);
   if (!guess) {
+    //No input added
     document.querySelector('.message').textContent = '⭕ No Number!';
   } else if (guess == secretNumber) {
+    //Correct Answer
     document.querySelector('.message').textContent = '🎉 Correct Number!';
-
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
     document.querySelector('.number').textContent = secretNumber;
   } else if (guess > secretNumber) {
+    //Answer Higher than Secret Number
     if (score > 0) {
       document.querySelector('.message').textContent = '😢 Too High!';
       score--;
@@ -30,6 +33,7 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.message').textContent = '🎃 You Lost the game!';
     }
   } else if (guess < secretNumber) {
+    //Answer Lower than Secret Number
     if (score > 0) {
       document.querySelector('.message').textContent = '😒 Too Low!';
       score--;
@@ -41,6 +45,7 @@ document.querySelector('.check').addEventListener('click', function () {
 });
 
 document.querySelector('.again').addEventListener('click', function () {
+  //reset button Funtion
   document.querySelector('body').style.backgroundColor = '#222';
   document.querySelector('.number').style.width = '15rem';
   document.querySelector('.message').textContent = 'Start guessing...';
