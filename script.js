51e9fb862a532c1e2fc303e7d93c78b80d8f9a26
hirusenevirathne <107979195+hirusenevirathne@users.noreply.github.com>
2022-12-27 05:50:42 +0530

    score method
    define scoring method and lost event too

--- script.js
+++ script.js
@@ -7,8 +7,10 @@
 // document.querySelector('.guess').value = 21;
 
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
 document.querySelector('.number').textContent = secretNumber;
 
+//Button click event
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(typeof guess);
@@ -17,8 +19,20 @@ document.querySelector('.check').addEventListener('click', function () {
   } else if (guess == secretNumber) {
     document.querySelector('.message').textContent = '🎉 Correct Number!';
   } else if (guess > secretNumber) {
-    document.querySelector('.message').textContent = '😢 Too High!';
+    if (score > 0) {
+      document.querySelector('.message').textContent = '😢 Too High!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '🎃 You Lost the game!';
+    }
   } else if (guess < secretNumber) {
-    document.querySelector('.message').textContent = '😒 Too Low!';
+    if (score > 0) {
+      document.querySelector('.message').textContent = '😒 Too Low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '🎃 You Lost the game!';
+    }
   }
 });
